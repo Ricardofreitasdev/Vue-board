2a9dab8e1c1379486aea41291c6c54e90b0e43d3
Ricardo Freitas <freitasdev7@gmail.com>
2022-12-03 19:29:17 -0300

    remove login

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,9 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   {
     path: "/",
-    name: "home",
-    redirect: "/login",
+    redirect: "/board",
   },
-  {
-    path: "/login",
-    name: "login",
-    component: () =>
-      import(/* webpackChunkName: "Login" */ "../views/Login.vue"),
-  },
+
   {
     path: "/board",
     name: "board",
@@ -20,22 +14,9 @@ const routes = [
   },
 ];
 
-function isAuthenticated() {
-  const hasToken = JSON.parse(localStorage.getItem("token"));
-  return hasToken ? true : false;
-}
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-});
-
-router.beforeEach((to, from, next) => {
-  if (to.name !== "login" && !isAuthenticated()) {
-    next({ name: "login" });
-  } else {
-    next();
-  }
 });
 
 export default router;
